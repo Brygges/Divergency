d4e984eb59bf5bd484fa76e9de73d0289bfe4836
Brygge <44093278+Brygges@users.noreply.github.com>
2025-04-14 09:05:33 +0200

    Add files via upload

--- flaskrun.py
+++ flaskrun.py
@@ -1,14 +1,20 @@
-from flask import Flask, send_file, abort
-import os
+from flask import Flask, send_file, abort, request
+import os, sys
 import webbrowser, threading
 
+if hasattr(sys, '_MEIPASS'):
+    # When bundled as onefile, PyInstaller extracts resources to sys._MEIPASS.
+    base_path = sys._MEIPASS
+else:
+    # Otherwise, use the directory of the executable/script.
+    base_path = os.path.dirname(os.path.abspath(__file__))
+
 app = Flask(__name__)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 @app.route('/')
 def index():
     # Serve the generated index.html file.
-    return send_file(os.path.join(BASE_DIR, "index.html"))
+    return send_file(os.path.join(base_path, "index.html"))
 
 @app.route('/shutdown', methods=['POST'])
 def shutdown():
@@ -20,11 +26,11 @@ def shutdown():
 
 @app.route('/data.json')
 def data_json():
-    return send_file(os.path.join(BASE_DIR, "data.json"))
+    return send_file(os.path.join(base_path, "data.json"))
 
 @app.route('/get_pdb/<node>')
 def get_pdb(node):
-    pdb_path = os.path.join(BASE_DIR, "AllSpecies", f"{node}.pdb")
+    pdb_path = os.path.join(base_path, "AllSpecies", f"{node}.pdb")
     if os.path.exists(pdb_path):
         return send_file(pdb_path, mimetype='text/plain')
     else:
@@ -32,7 +38,7 @@ def get_pdb(node):
 
 @app.route('/superposed/<path:filename>')
 def serve_superposed(filename):
-    super_path = os.path.join(BASE_DIR, "Superposed", f"{filename}")
+    super_path = os.path.join(base_path, "Superposed", f"{filename}")
     if os.path.exists(super_path):
         return send_file(super_path, mimetype='text/plain')
     else:
@@ -44,4 +50,4 @@ def open_browser():
 
 if __name__ == '__main__':
     threading.Thread(target=open_browser).start()
-    app.run(debug=True)
+    app.run(debug=False)
